149f39e97f9278b323607a590953128fb7477c58
Divy1207 <105902252+Divy1207@users.noreply.github.com>
2025-01-11 12:10:45 -0500

    Updated Glue Script

--- Codes/S3_hudi_data_lake.py
+++ Codes/S3_hudi_data_lake.py
@@ -1,10 +1,12 @@
 import sys
+import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+# Parse job arguments
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -12,15 +14,56 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node Amazon S3
-S3_data_node = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://test-divy/datasets/payment.csv"], "recurse": True}, transformation_ctx="AmazonS3_node1736611043146")
+# Read data from Amazon S3
+input_s3_path = "s3://test-divy/datasets/payment.csv"
+AmazonS3_source = glueContext.create_dynamic_frame.from_options(
+    format_options={
+        "quoteChar": "\"",
+        "withHeader": True,
+        "separator": ",",
+        "optimizePerformance": False
+    },
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": [input_s3_path],
+        "recurse": True
+    },
+    transformation_ctx="AmazonS3_source"
+)
 
-# Script generated for node Amazon S3
-additional_options = {"hoodie.table.name": "my_hudi_table_trial", "hoodie.datasource.write.table.type": "COPY_ON_WRITE", "hoodie.datasource.write.operation": "insert", "hoodie.datasource.write.recordkey.field": "payment_id", "hoodie.datasource.write.precombine.field": "order_id", "hoodie.datasource.write.hive_style_partitioning": "true", "hoodie.parquet.compression.codec": "gzip", "hoodie.datasource.hive_sync.enable": "true", "hoodie.datasource.hive_sync.database": "default", "hoodie.datasource.hive_sync.table": "hudi_test", "hoodie.datasource.hive_sync.partition_extractor_class": "org.apache.hudi.hive.MultiPartKeysValueExtractor", "hoodie.datasource.hive_sync.use_jdbc": "false", "hoodie.datasource.hive_sync.mode": "hms"}
-AmazonS3_node1736611082945_df = AmazonS3_node1736611043146.toDF()
-AmazonS3_node1736611082945_df.write.format("hudi").options(**additional_options).mode("append").save("s3://testing-hudi-divy/myTest/")
+# Define Hudi options
+hudi_options = {
+    # Table configuration
+    "hoodie.table.name": "my_hudi_table_trial2",
+    "hoodie.datasource.write.table.type": "MERGE_ON_READ",
+    "hoodie.datasource.write.operation": "insert",
+    
+    # Key and partition configuration
+    "hoodie.datasource.write.recordkey.field": "payment_id",
+    "hoodie.datasource.write.precombine.field": "order_id",
+    "hoodie.datasource.write.hive_style_partitioning": "true",
+    
+    # Compression settings
+    "hoodie.parquet.compression.codec": "gzip",
+    
+    # Hive sync settings
+    "hoodie.datasource.hive_sync.enable": "true",
+    "hoodie.datasource.hive_sync.database": "default",
+    "hoodie.datasource.hive_sync.table": "hudi_test_2",
+    "hoodie.datasource.hive_sync.partition_extractor_class": "org.apache.hudi.hive.MultiPartKeysValueExtractor",
+    "hoodie.datasource.hive_sync.use_jdbc": "false",
+    "hoodie.datasource.hive_sync.mode": "hms"
+}
 
+# Write data to Hudi
+output_s3_path = "s3://testing-hudi-divy/myTest/"
+AmazonS3_source_df = AmazonS3_source.toDF()
+AmazonS3_source_df.write.format("hudi").options(**hudi_options).mode("append").save(output_s3_path)
+
+# Commit the job
 job.commit()
+
 
 #Note
 #Hudi Parameters to mention in Glue job Parameters
